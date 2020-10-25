ddc136830446da6a6e4d062a2614a9866cfdd00a
Taieba-Sadeqi <taieba.sadeqi@gmail.com>
2020-10-24 21:03:50 -0700

    test

---
+++ src/test/java/stepDefinitions/RegisterFormStepDefinition.java
@@ -0,0 +1,45 @@
+package stepDefinitions;
+
+import core.Base;
+import cucumber.api.DataTable;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RegisterFormStepDefinition extends Base {
+	
+	
+	@When("^User click on Register$")
+	public void user_click_on_Register() throws Throwable {
+
+	}
+
+	@When("^User fill out Register form with below information$")
+	public void user_fill_out_Register_form_with_below_information(DataTable arg1) throws Throwable {
+
+
+	}
+
+	@When("^User select 'yes' for Subscribe$")
+	public void user_select_yes_for_Subscribe() throws Throwable {
+
+	}
+
+	@When("^User click on privacy and policy check box$")
+	public void user_click_on_privacy_and_policy_check_box() throws Throwable {
+
+	}
+
+	@When("^User click on Continue button$")
+	public void user_click_on_Continue_button() throws Throwable {
+
+	}
+
+	@Then("^User Should see message '(.+)'$")
+	public void user_Should_see_message_Your_Account_Has_Been_Created(String message) throws Throwable {
+
+	}
+
+	
+	
+
+}
