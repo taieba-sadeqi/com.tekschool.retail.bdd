9862bd5b5aea79166915ccb8f45f006beef97be2
Taieba Sadeqi <Taieba.sadeqi@gmail.com>
2020-11-08 23:43:37 -0800

    check my homework thanks

--- target/cucumber-reports/cucumber-pretty/report.js
+++ target/cucumber-reports/cucumber-pretty/report.js
@@ -1,73 +1,91 @@
-$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Features/DatabaseFeature.feature");
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("Features/Currency.feature");
 formatter.feature({
   "line": 1,
-  "name": "Database SQL query feature",
+  "name": "Testing of Currency in testing retail website",
   "description": "",
-  "id": "database-sql-query-feature",
+  "id": "testing-of-currency-in-testing-retail-website",
   "keyword": "Feature"
 });
 formatter.before({
-  "duration": 5295656800,
+  "duration": 5911072100,
   "status": "passed"
 });
 formatter.scenario({
   "line": 4,
-  "name": "Execute all information from Actor Table",
+  "name": "Check diffrent currency in all product",
   "description": "",
-  "id": "database-sql-query-feature;execute-all-information-from-actor-table",
+  "id": "testing-of-currency-in-testing-retail-website;check-diffrent-currency-in-all-product",
   "type": "scenario",
   "keyword": "Scenario",
   "tags": [
     {
       "line": 3,
-      "name": "@sqlquery"
+      "name": "@currency"
     }
   ]
 });
 formatter.step({
-  "line": 6,
-  "name": "User connects to PostgreSQL",
+  "line": 5,
+  "name": "user should be able see price of my products in different currency",
   "keyword": "Given "
 });
 formatter.step({
+  "line": 6,
+  "name": "User should see the default currency which is dollar sign",
+  "keyword": "When "
+});
+formatter.step({
   "line": 7,
-  "name": "User sends \"select * from public.actor\"",
+  "name": "user click on drop down on top left side of webpage",
   "keyword": "When "
 });
 formatter.step({
   "line": 8,
-  "name": "User should get all information from that table",
+  "name": "user select any other currency",
+  "keyword": "And "
+});
+formatter.step({
+  "line": 9,
+  "name": "user should be able to see items with diffrent currency",
   "keyword": "Then "
 });
 formatter.match({
-  "location": "SqlQueryStepDefinition.user_connects_to_PostgreSQL()"
+  "location": "CurrencyStepDefination.user_should_be_able_see_price_of_my_products_in_different_currency()"
 });
 formatter.result({
-  "duration": 243500100,
+  "duration": 2435316500,
   "status": "passed"
 });
 formatter.match({
-  "arguments": [
-    {
-      "val": "select * from public.actor",
-      "offset": 12
-    }
-  ],
-  "location": "SqlQueryStepDefinition.user_sends(String)"
+  "location": "CurrencyStepDefination.user_should_see_the_default_currency_which_is_dollar_sign()"
 });
 formatter.result({
-  "duration": 27284900,
+  "duration": 38029500,
   "status": "passed"
 });
 formatter.match({
-  "location": "SqlQueryStepDefinition.user_should_get_all_information_from_that_table()"
+  "location": "CurrencyStepDefination.user_click_on_drop_down_on_top_left_side_of_webpage()"
 });
 formatter.result({
-  "duration": 75207000,
+  "duration": 110900,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CurrencyStepDefination.user_select_any_other_currency()"
+});
+formatter.result({
+  "duration": 111389000,
+  "status": "passed"
+});
+formatter.match({
+  "location": "CurrencyStepDefination.user_should_be_able_to_see_items_with_diffrent_currency()"
+});
+formatter.result({
+  "duration": 425044400,
   "status": "passed"
 });
 formatter.after({
-  "duration": 720204700,
+  "duration": 991933800,
   "status": "passed"
 });
 });
